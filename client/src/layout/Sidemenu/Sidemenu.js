19a1091df65f7668ce1aa22229f0d0ae03b676aa
Adam Chmara <adam.chmara1@gmail.com>
2020-02-14 17:57:26 +0100

    Add navigation items, sidemenu changes
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- client/src/layout/Sidemenu/Sidemenu.js
+++ client/src/layout/Sidemenu/Sidemenu.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Logo from "../../common/Logo/Logo";
 import "./Sidemenu.css";
-import { Icon } from "semantic-ui-react";
+import { Icon, Divider } from "semantic-ui-react";
 
 const nav = [
   {
@@ -10,7 +10,42 @@ const nav = [
   },
   {
     title: "History",
-    icon: "circle"
+    icon: "history"
+  },
+  {
+    title: "Login Data",
+    icon: "key"
+  },
+  {
+    title: "Web Data",
+    icon: "globe"
+  },
+  {
+    title: "Downloads",
+    icon: "cloud download"
+  },
+  {
+    title: "Bookmarks",
+    icon: "bookmark"
+  },
+  {
+    title: "Favicons",
+    icon: "image"
+  },
+  {
+    title: "Cache",
+    icon: "folder open"
+  }
+];
+
+const navAlt = [
+  {
+    title: "Volume",
+    icon: "hdd"
+  },
+  {
+    title: "Database",
+    icon: "database"
   }
 ];
 
@@ -21,8 +56,14 @@ function Sidemenu() {
     <div className="sidemenu">
       <div className="logo">
         <Logo color="#6F9CEB" />
+        <p style={{ color: "#6F9CEB", marginTop: "-20px" }}>
+          Google Chrome forensics tool
+        </p>
       </div>
       <div className="nav">
+        <Divider horizontal inverted>
+          Navigation
+        </Divider>{" "}
         {nav.map((el, i) => (
           <div
             key={i}
@@ -30,7 +71,25 @@ function Sidemenu() {
             className={`item ${selected === i ? "selected" : null}`}
           >
             <p>
-              <Icon name={el.icon} />
+              <Icon name={el.icon} style={{ marginRight: "20px" }} />
+              &nbsp;
+              {el.title}
+            </p>
+          </div>
+        ))}
+        <Divider horizontal inverted>
+          More
+        </Divider>
+        {navAlt.map((el, i) => (
+          <div
+            key={i + nav.length}
+            onClick={() => setSelected(i + nav.length)}
+            className={`item ${
+              selected === i + nav.length ? "selected" : null
+            }`}
+          >
+            <p>
+              <Icon name={el.icon} style={{ marginRight: "20px" }} />
               &nbsp;
               {el.title}
             </p>
